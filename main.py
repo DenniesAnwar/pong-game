48cbdc7a483db5927090d0a5becee8634b43cb5b
Dennies PC <denniesanwar27@gmail.com>
2024-04-27 22:06:33 -0400

    Pong mechanics works; paddle able to hit ball back & bounces when ball collide with wall

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+import time
 from turtle import Turtle, Screen
 from paddle import Paddle
-
-# Create Paddle Class
-# Create Scoreboard Class
-# Create Ball Class
+from ball import Ball
 
 RIGHT_POS = (350, 0)
 LEFT_POS = (-350, 0)
@@ -17,7 +15,7 @@ screen.tracer(0)
 
 right_paddle = Paddle(RIGHT_POS)
 left_paddle = Paddle(LEFT_POS)
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
@@ -28,6 +26,20 @@ screen.onkey(left_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(.09)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320:
+        ball.rally()
+
+    if ball.distance(left_paddle) < 50 and ball.xcor() < -325:
+        ball.rally()
+
+
 
 screen.exitonclick()
